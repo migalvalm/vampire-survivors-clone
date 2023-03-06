be596d37abe84a5fc152b30ac020e20c00c924f5
migalvalm <59445459+migalvalm@users.noreply.github.com>
2023-03-06 01:24:44 +0000

    Section 1
    - Setup godot settings
    - Creating the player
    - Player movement
    - Creating a Tilemap
    - Game Camera

---
+++ scenes/game_camera/game_camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+func _ready() -> void:
+	make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
